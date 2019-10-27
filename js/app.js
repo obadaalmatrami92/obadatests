f661738bf5b625256f7270f2b30a95d950d01162
obada almatrami <obada.matrami92@gmail.com>
2019-10-27 23:01:06 +0200

    edit

--- js/app.js
+++ js/app.js
@@ -40,13 +40,36 @@ function showScores() {
     element.innerHTML = gameOverHTML;
 };
 
+function startagain() {
+    window.history.back();
+};
+
 // create questions
 var questions = [
+
     new Question("Which one is not an object oriented programming language?", ["Java", "C#", "C++", "C"], "C"),
     new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
     new Question("There are ____ main components of object oriented programming.", ["1", "6", "2", "4"], "4"),
     new Question("Which language is used for web apps?", ["PHP", "Python", "Javascript", "All"], "All"),
-    new Question("MVC is a ____.", ["Language", "Library", "Framework", "All"], "Framework")
+    new Question("MVC is a ____.", ["Language", "Library", "Framework", "All"], "Framework"),
+    new Question("Which one is not an object oriented programming language?", ["Java", "C#", "C++", "C"], "C"),
+    new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
+    new Question("There are ____ main components of object oriented programming.", ["1", "6", "2", "4"], "4"),
+    new Question("Which language is used for web apps?", ["PHP", "Python", "Javascript", "All"], "All"),
+    new Question("MVC is a ____.", ["Language", "Library", "Framework", "All"], "Framework"),
+    new Question("Which one is not an object oriented programming language?", ["Java", "C#", "C++", "C"], "C"),
+    new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
+    new Question("There are ____ main components of object oriented programming.", ["1", "6", "2", "4"], "4"),
+    new Question("Which language is used for web apps?", ["PHP", "Python", "Javascript", "All"], "All"),
+    new Question("MVC is a ____.", ["Language", "Library", "Framework", "All"], "Framework"),
+    new Question("Which one is not an object oriented programming language?", ["Java", "C#", "C++", "C"], "C"),
+    new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
+    new Question("There are ____ main components of object oriented programming.", ["1", "6", "2", "4"], "4"),
+    new Question("Which language is used for web apps?", ["PHP", "Python", "Javascript", "All"], "All"),
+    new Question("MVC is a ____.", ["Language", "Library", "Framework", "All"], "Framework"),
+
+
+
 ];
 
 // create quiz
